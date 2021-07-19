71f308b359762506b83de13e308db689dc74b47f
ihabarbid <86122391+ihabarbid@users.noreply.github.com>
2021-07-19 23:16:32 +0300

    Add files via upload

---
+++ 0-project_add_new.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _0_project_add_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("$ 0. Project Window: Adding Assets Answer");
+            Console.WriteLine("3");
+            Console.WriteLine("$");
+        }
+    }
+}
